4dc3558b45f9f1498f5325fe420074892ceffa7f
Nick Laros <nicklaros@gmail.com>
2021-01-27 17:59:37 +0700

    CTE fragments and custom value fragment on create query builder

--- create.go
+++ create.go
@@ -7,10 +7,12 @@ import (
 )
 
 type createQueryBuilder struct {
-	intoFragment       string
-	fieldFragments     []string
-	valueFragments     []string
-	onConflictFragment string
+	cteFragments            []string
+	intoFragment            string
+	fieldFragments          []string
+	valueFragments          []string
+	valueWithSelectFragment string
+	onConflictFragment      string
 }
 
 // NewCreateQuery creates new sql builder instance for insert operation.
@@ -20,6 +22,13 @@ func NewCreateQuery(table string) *createQueryBuilder {
 	}
 }
 
+// AddCTE adds common table expression to include in generated query.
+func (ths *createQueryBuilder) AddCTE(CTEs ...string) *createQueryBuilder {
+	ths.cteFragments = append(ths.cteFragments, CTEs...)
+
+	return ths
+}
+
 // AddField adds field to insert in generated query.
 func (ths *createQueryBuilder) AddField(fields ...string) *createQueryBuilder {
 	ths.fieldFragments = append(ths.fieldFragments, fields...)
@@ -27,9 +36,16 @@ func (ths *createQueryBuilder) AddField(fields ...string) *createQueryBuilder {
 	return ths
 }
 
-// AddValue adds field to insert in generated query.
+// AddValue adds value to insert in generated query.
 func (ths *createQueryBuilder) AddValue(values ...string) *createQueryBuilder {
 	ths.valueFragments = append(ths.valueFragments, values...)
+
+	return ths
+}
+
+// AddValueWithSelect adds value with select in generated query.
+func (ths *createQueryBuilder) AddValueWithSelect(valueWithSelect string) *createQueryBuilder {
+	ths.valueWithSelectFragment = valueWithSelect
 
 	return ths
 }
@@ -49,18 +65,38 @@ func (ths *createQueryBuilder) BuildQuery() (string, error) {
 	if len(ths.fieldFragments) == 0 {
 		return "", errors.New("no field specified, add it using AddField method")
 	}
-	if len(ths.valueFragments) == 0 {
-		return "", errors.New("no value to be inserted, add it using AddValue method")
+	if len(ths.valueFragments) == 0 && len(ths.valueWithSelectFragment) == 0 {
+		return "", errors.New("no value(s) to be inserted, add it using AddValue or AddValueWithSelect method")
+	}
+	if len(ths.valueFragments) > 0 && len(ths.valueWithSelectFragment) > 0 {
+		return "", errors.New("use only AddValue or AddValueWithSelect to add value(s)")
 	}
 
 	queryFragments := []string{}
 
+	if len(ths.cteFragments) > 0 {
+		queryFragments = append(queryFragments, fmt.Sprintf(
+			"WITH %s",
+			strings.Join(ths.cteFragments, ", "),
+		))
+	}
+
 	queryFragments = append(queryFragments, fmt.Sprintf(
-		"INSERT INTO %s (%s) VALUES %s",
+		"INSERT INTO %s (%s)",
 		ths.intoFragment,
 		strings.Join(ths.fieldFragments, ", "),
-		strings.Join(ths.valueFragments, ", "),
 	))
+
+	if len(ths.valueFragments) > 0 {
+		queryFragments = append(queryFragments, fmt.Sprintf(
+			"VALUES %s",
+			strings.Join(ths.valueFragments, ", "),
+		))
+	}
+
+	if len(ths.valueWithSelectFragment) > 0 {
+		queryFragments = append(queryFragments, ths.valueWithSelectFragment)
+	}
 
 	if len(ths.onConflictFragment) > 0 {
 		queryFragments = append(queryFragments, ths.onConflictFragment)
--- create_test.go
+++ create_test.go
@@ -35,7 +35,7 @@ func TestNewCreateQuery(t *testing.T) {
 
 		Convey("It should returns error", func() {
 			So(err, ShouldNotBeNil)
-			So(err.Error(), ShouldEqual, "no value to be inserted, add it using AddValue method")
+			So(err.Error(), ShouldEqual, "no value(s) to be inserted, add it using AddValue or AddValueWithSelect method")
 			So(query, ShouldEqual, "")
 		})
 	})
@@ -87,6 +87,64 @@ func TestNewCreateQuery(t *testing.T) {
 		Convey("It should returns generated query", func() {
 			So(err, ShouldBeNil)
 			So(query, ShouldEqual, `INSERT INTO table_a (field_1, field_2) VALUES (value_1, value_2), (value_3, value_4), (value_5, value_6) ON CONFLICT ON CONSTRAINT table_a_pkey DO UPDATE SET field_2 = EXCLUDED.field_2`)
+		})
+	})
+
+	Convey("If AddValue and AddValueWithSelect used in the same time", t, func() {
+		query, err := NewCreateQuery("table_a").
+			AddField("field_1", "field_2").
+			AddValue(
+				"(value_1, value_2)",
+				"(value_3, value_4)",
+				"(value_5, value_6)",
+			).
+			AddValueWithSelect(`
+				SELECT
+					a,
+					b`,
+			).
+			BuildQuery()
+
+		Convey("It should returns error", func() {
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldEqual, "use only AddValue or AddValueWithSelect to add value(s)")
+			So(query, ShouldEqual, "")
+		})
+	})
+
+	Convey("CTEs added", t, func() {
+		query, err := NewCreateQuery("table_a").
+			AddCTE(
+				`cte_1 AS (SELECT 1)`,
+				`cte_2 AS (SELECT 2)`,
+			).
+			AddField("field_1", "field_2").
+			AddValue(
+				"(value_1, value_2)",
+				"(value_3, value_4)",
+				"(value_5, value_6)",
+			).
+			BuildQuery()
+
+		Convey("It should returns generated query", func() {
+			So(err, ShouldBeNil)
+			So(query, ShouldEqual, `WITH cte_1 AS (SELECT 1), cte_2 AS (SELECT 2) INSERT INTO table_a (field_1, field_2) VALUES (value_1, value_2), (value_3, value_4), (value_5, value_6)`)
+		})
+	})
+
+	Convey("Input value with AddValueWithSelect", t, func() {
+		query, err := NewCreateQuery("table_a").
+			AddField("field_1", "field_2").
+			AddValueWithSelect(`
+				SELECT
+					a,
+					b`,
+			).
+			BuildQuery()
+
+		Convey("It should returns generated query", func() {
+			So(err, ShouldBeNil)
+			So(query, ShouldEqual, `INSERT INTO table_a (field_1, field_2) SELECT a, b`)
 		})
 	})
 }
